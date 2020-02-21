e026b5fffdd3ef7cf99bcfaeeb0a1ad201f38ebb
Frcsty <50297792+Frcsty@users.noreply.github.com>
2020-02-21 16:26:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var app = new Vue({ 
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
+var app = new Vue({
+  el: '#app',
+  data: {
+    showMessage: false;
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    showProjects: function () {
+      this.showMessage = true;
     }
-});
+  }
+})
